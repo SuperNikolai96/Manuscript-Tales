14b94178dfa12b5c2f7518a517e269b49d4dcc1a
Nikolai Jónasson <a.jono.niko.studio@gmail.com>
2023-03-15 16:37:32 +0200

    Added basic collectible script for key bits

---
+++ Assets/Bluebeard/Collectible.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public static event Action OnCollected;
+  
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(90f, Time.time * 100f, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnCollected?.Invoke();
+            Destroy(gameObject); 
+        }
+    }
+}

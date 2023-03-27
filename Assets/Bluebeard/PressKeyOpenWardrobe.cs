8bfb708bab77ac56794534c4815a3a71e6743844
Nikolai Jónasson <a.jono.niko.studio@gmail.com>
2023-03-27 22:22:20 +0300

    Imported animated wardrobe spirit

---
+++ Assets/Bluebeard/PressKeyOpenWardrobe.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PressKeyOpenWardrobe : MonoBehaviour
+{
+    public GameObject Instruction;
+    public GameObject AnimeObject;
+    public GameObject ThisTrigger;
+    public AudioSource DoorOpenSound;
+    public bool Action = false;
+
+    void Start()
+    {
+        Instruction.SetActive(false);
+
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Instruction.SetActive(true);
+            Action = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        Instruction.SetActive(false);
+        Action = false;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Action == true)
+            {
+                Instruction.SetActive(false);
+                AnimeObject.GetComponent<Animator>().Play("WardrobeAction");
+                ThisTrigger.SetActive(false);
+                DoorOpenSound.Play();
+                Action = false;
+            }
+        }
+
+    }
+}
